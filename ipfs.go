29f694ad6bfb95e06da0e5c58c43fa5ce2f2f503
Sourabh Niyogi <sourabh@wolk.com>
2018-10-06 15:57:05 -0700

    ipfshash

---
+++ ipfs.go
@@ -0,0 +1,27 @@
+package dns3
+
+import (
+	"fmt"
+	"log"
+	"os/exec"
+
+	"github.com/btcsuite/btcutil/base58"
+)
+
+func IPFSHashToBytes(h string) (hashtype uint8, digest []byte, err error) {
+	ipfs := base58.Decode(h)
+	// 12 20 8bd215d69eef287f6cc093501672d6b1f8c908bf5adedc47993d5676e39577b6
+	hashtype = ipfs[0]
+	sz := int(ipfs[1])
+	digest = ipfs[2:]
+	if len(digest) != sz {
+		return hashtype, digest, fmt.Errorf("incorrect size")
+	}
+	return hashtype, digest, nil
+}
+
+func submitZone(fn string) {
+	cmd := exec.Command("ipfs", "add", fn)
+	err := cmd.Run()
+	log.Printf("Command finished with error: %v", err)
+}
---
+++ ipfs_test.go
@@ -0,0 +1,68 @@
+package dns3
+
+import (
+	"fmt"
+	"sync"
+	"testing"
+	"time"
+
+	"github.com/miekg/dns"
+)
+
+func AnotherHelloServer(w ResponseWriter, req *dns.Msg) {
+	m := new(dns.Msg)
+	m.SetReply(req)
+
+	m.Extra = make([]dns.RR, 1)
+	m.Extra[0] = &dns.TXT{Hdr: RR_Header{Name: m.Question[0].Name, Rrtype: TypeTXT, Class: ClassINET, Ttl: 0}, Txt: []string{"Hello example"}}
+	w.WriteMsg(m)
+}
+
+func TestServingListenAndServe(t *testing.T) {
+	HandleFunc("example.com.", AnotherHelloServer)
+	defer HandleRemove("example.com.")
+
+	waitLock := sync.Mutex{}
+	server := &dns.Server{Addr: ":0", Net: "udp", ReadTimeout: time.Hour, WriteTimeout: time.Hour, NotifyStartedFunc: waitLock.Unlock}
+	waitLock.Lock()
+
+	go func() {
+		server.ListenAndServe()
+	}()
+	waitLock.Lock()
+
+	c, m := new(dns.Client), new(dns.Msg)
+	m.SetQuestion("example.com.", dns.TypeTXT)
+	addr := server.PacketConn.LocalAddr().String() // Get address via the PacketConn that gets set.
+	r, _, err := c.Exchange(m, addr)
+	if err != nil {
+		t.Fatal("failed to exchange example.com", err)
+	}
+	txt := r.Extra[0].(*dns.TXT).Txt[0]
+	if txt != "Hello example" {
+		t.Error("unexpected result for example.com", txt, "!= Hello example")
+	}
+	server.Shutdown()
+}
+
+func TestIPFSHashToBytes(t *testing.T) {
+	sample := "QmXkTBPtuJ1pTYRQ1U4AsSgAy1vE7r1EaMSAJ4pKMkZj89"
+	hashtype, digest, err := IPFSHashToBytes(sample)
+	if err != nil {
+		t.Fatalf("IPFSHashToBytes %v", err)
+	}
+	fmt.Printf("TestIPFS: %d %x (%d bytes)\n", hashtype, digest, len(digest))
+}
+
+/*
+func TestRegisterDomain(t *testing.T) {
+	domain := "eth.hacker"
+	RegisterDomain(domain)
+}
+
+func TestSubmitZone(t *testing.T) {
+	domain := "eth.hacker"
+	zoneHash := "QmXkTBPtuJ1pTYRQ1U4AsSgAy1vE7r1EaMSAJ4pKMkZj89"
+	SubmitZone(domain, zoneHash)
+}
+*/
